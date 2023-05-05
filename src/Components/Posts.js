50c29c54c0a46506569b3858c39ab1ed46e12ad0
Alysson Henrique <alyssonhra99@gmail.com>
2023-05-05 13:48:19 -0300

    feat: posts component refactored (each post has it's own state --> only re-render the one which changes

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -25,146 +25,120 @@ const postsData = [...posts, ...posts].map((post, index) => (
         userNameLike: post[4],
         numberOfLikes: Math.ceil(Math.random() * 1000),
         id: index,
-        liked: false,
     }
 ));
 // -----------------------------
 // Components:
-
-const PostHead = ({ imgSrc, author }) => {
-    return (
-        <div className="post-head">
-            <div className="left">
-                <img src={imgSrc} alt="post-author" style={{ width: "32px", height: "32px" }} />
-                <h1 style={{
-                    fontWeight: 500,
-                    fontSize: '14px',
-                    color: '#262626',
-                }}>{author}</h1>
-            </div>
-            <img src={ellipsis} alt="more-options" className='ion-icon' />
-        </div>
-    );
-};
-
-const PostImg = ({ imgPost, postId, isLiked, setPostsState }) => {
-    const [dbClickLike, setDbClickLike] = useState('hidden');
-    const handleLikeImg = (id) => {
-        if (isLiked) return;
-        setDbClickLike('');
-        setTimeout(() => {
-            setDbClickLike('hidden');
-        }, 500);
-        setPostsState(prevState => (
-            prevState.map(post => {
-                if (post.id === id) {
-                    post.numberOfLikes++;
-                    post.liked = true;
-                }
-                return post;
-            })
-        ))
-    };
-
-    return (
-        <div className="post-img">
-            <img
-                src={imgPost}
-                alt="post-img"
-                onDoubleClick={() => handleLikeImg(postId)}
-                data-test="post-image"
-                className='post-img'
-            />
-            <div className={`animation ${dbClickLike}`}>
-                <ion-icon name="heart"></ion-icon>
-            </div>
-        </div>
-    );
-};
-
-const PostFooter = ({ nameUserLike, imgUserLike, numberOfLikes, postId, isLiked, setPostsState }) => {
+const Post = ({ userImgUrl, author, imgPost, imgUserLike, nameUserLike, numberOfLikes }) => {
+    const [isLiked, setIsLiked] = useState(false);
+    const [isDbClick, setIsDbClick] = useState(false);
+    const [heartIcon, setHeartIcon] = useState('heart-outline');
     const [savePost, setSavePost] = useState('bookmark-outline');
+    const [nLikes, setNLikes] = useState(numberOfLikes);
 
-    const handleLikeHeart = (id) => {
-        setPostsState(prevState => {
-            return prevState.map(post => {
-                if (post.id === id) {
-                    isLiked ? post.numberOfLikes-- : post.numberOfLikes++;
-                    post.liked = !isLiked;
-                }
-                return post;
-            })
-        })
+    const handleLike = (dbClick = false) => {
+        if (dbClick && isLiked) return;
+        if (isLiked) {
+            setNLikes(prevState => prevState - 1);
+            setHeartIcon('heart-outline');
+            setIsLiked(false);
+        } else {
+            setNLikes(prevState => prevState + 1);
+            setHeartIcon('heart');
+            setIsLiked(true);
+            if (dbClick) {
+                setIsDbClick(true);
+                setTimeout(() => {
+                    setIsDbClick(false);
+                }, 500);
+            }
+        };
     };
 
     return (
-        <div className="post-footer">
-            <div className="actions">
+        <>
+            <div className="post-head">
                 <div className="left">
-                    <ion-icon
-                        className="heart-like"
-                        name={isLiked ? 'heart' : 'heart-outline'}
-                        style={isLiked ? { color: 'red' } : {}}
-                        onClick={() => handleLikeHeart(postId)}
-                        data-test="like-post"
-                    ></ion-icon>
-                    <img src={chat} alt="comment" className='ion-icon' />
-                    <img src={plane} alt="share" className='ion-icon' />
-                </div>
-                <div className="right">
-                    <ion-icon
-                        name={savePost}
-                        onClick={
-                            () => savePost === 'bookmark'
-                                ? setSavePost('bookmark-outline')
-                                : setSavePost('bookmark')
-                        }
-                        data-test="save-post"
-                    ></ion-icon>
-                </div>
-            </div>
-            <div className="info-likes">
-                <img src={imgUserLike} alt="likes" className='ion-icon' />
-                <h4
-                    style={{
-                        fontWeight: 300,
+                    <img src={userImgUrl} alt="post-author" style={{ width: "32px", height: "32px" }} />
+                    <h1 style={{
+                        fontWeight: 500,
                         fontSize: '14px',
-                        color: '#000'
-                    }}
-                >
-                    Curtido por <span className='strong'>
-                        {nameUserLike}
-                    </span> e <span className='strong'>
-                        outras <span data-test="likes-number">
-                            {numberOfLikes} </span> pessoas
-                    </span>
-                </h4>
+                        color: '#262626',
+                    }}>{author}</h1>
+                </div>
+                <img src={ellipsis} alt="more-options" className='ion-icon' />
             </div>
-        </div>
+            <div className="post-img">
+                <img
+                    src={imgPost}
+                    alt="post-img"
+                    onDoubleClick={() => handleLike(true)}
+                    data-test="post-image"
+                    className='post-img'
+                />
+                {isDbClick && <div className='animation'>
+                    <ion-icon name="heart"></ion-icon>
+                </div>}
+            </div>
+            <div className="post-footer">
+                <div className="actions">
+                    <div className="left">
+                        <ion-icon
+                            name={heartIcon}
+                            style={isLiked ? { color: 'red' } : { color: 'black' }}
+                            onClick={() => handleLike()}
+                            data-test="like-post"
+                        ></ion-icon>
+                        <img src={chat} alt="comment" className='ion-icon' />
+                        <img src={plane} alt="share" className='ion-icon' />
+                    </div>
+                    <div className="right">
+                        <ion-icon
+                            name={savePost}
+                            onClick={
+                                () => savePost === 'bookmark'
+                                    ? setSavePost('bookmark-outline')
+                                    : setSavePost('bookmark')
+                            }
+                            data-test="save-post"
+                        ></ion-icon>
+                    </div>
+                </div>
+                <div className="info-likes">
+                    <img src={imgUserLike} alt="likes" className='ion-icon' />
+                    <h4
+                        style={{
+                            fontWeight: 300,
+                            fontSize: '14px',
+                            color: '#000'
+                        }}
+                    >
+                        Curtido por <span className='strong'>
+                            {nameUserLike}
+                        </span> e <span className='strong'>
+                            outras <span data-test="likes-number">
+                                {nLikes} </span> pessoas
+                        </span>
+                    </h4>
+                </div>
+            </div>
+        </>
     );
-}
+};
 
 const Posts = () => {
-    const [postsState, setPostsState] = useState(postsData);
 
     return (
         <div className='posts'>
-            {postsState.map((post) => (
+            {postsData.map((post) => (
                 <div className="post" key={`post-${post.id}`} data-test="post">
-                    <PostHead imgSrc={post.imgUrl} author={post.user} />
-                    <PostImg
+                    <Post
+                        userImgUrl={post.imgUrl}
+                        author={post.user}
                         imgPost={post.imgPost}
-                        postId={post.id}
-                        isLiked={post.liked}
-                        setPostsState={setPostsState}
-                    />
-                    <PostFooter
-                        nameUserLike={post.userNameLike}
                         imgUserLike={post.imgUserLike}
+                        nameUserLike={post.userNameLike}
                         numberOfLikes={post.numberOfLikes}
-                        postId={post.id}
-                        isLiked={post.liked}
-                        setPostsState={setPostsState}
                     />
                 </div>
             ))}
